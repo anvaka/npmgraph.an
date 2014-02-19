adaddf36cca05024d68bce549febcc7957b5962e
anvaka <anvaka@gmail.com>
2014-02-19 01:25:25 -0800

    Updated to use `an` module

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-var appName = 'sj-app';
-var module = angular.module(appName, []);
-var compileProvider;
-var compiledDirectives = {};
+require('./lib/navbar');
 
-module.config(function($compileProvider) {
-  compileProvider = $compileProvider;
-});
-
-module.directive('require', function ($compile) {
-  return {
-    link: function (scope, element, attrs) {
-      var directiveName = element[0].localName;
-      if (compiledDirectives[directiveName]) return;
-
-      var requireName = attrs.require;
-      var targetDirective = require(requireName);
-      if (compileProvider) {
-        compileProvider.directive(directiveName, targetDirective);
-        compiledDirectives[directiveName] = 1;
-        $compile(element)(scope);
-      }
-    }
-  };
-});
-
-angular.bootstrap(document, [appName]);
+require('an').flush();
