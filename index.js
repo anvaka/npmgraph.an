a3a3ea17e4ee370fc0bea3340e5dd90a4a2de38d
anvaka <anvaka@gmail.com>
2014-02-21 01:00:35 -0800

    It is possible to use vanila angular too
    This commit shows how to use regular angular module, which is written
    with `an` wrapper

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ require('./lib/navbar');
 require('./lib/mainController');
 
 // bootstrap angular application:
-require('an').flush();
+npmVizApp = angular.module('npmViz', ['ngRoute']);
+npmVizApp.config(require('./lib/routes'));
+
+require('an').flush(npmVizApp);
+
+angular.bootstrap(document, [npmVizApp.name]);
