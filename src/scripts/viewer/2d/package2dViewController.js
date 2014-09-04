34fef27b2bd0169e38ca2cd33ab2591f60a55a15
anvaka <anvaka@gmail.com>
2014-09-04 00:54:55 -0700

    Autoselect root

--- src/scripts/viewer/2d/package2dViewController.js
+++ src/scripts/viewer/2d/package2dViewController.js
@@ -3,28 +3,38 @@
  */
 require('./graphViewer');
 
-module.exports = function ($scope, $routeParams, $http, $location) {
+module.exports = function($scope, $routeParams, $http, $location) {
   $scope.name = ' ' + $routeParams.pkgId;
   $scope.root = $routeParams.pkgId;
   $scope.onNodeSelected = selectPackage;
-  $scope.switchMode = function () {
+  $scope.switchMode = function() {
     $location.path('view/3d/' + $routeParams.pkgId);
   };
 
   var graphBuilder = require('../graphBuilder')($routeParams.pkgId, $http);
   $scope.graph = graphBuilder.graph;
-  graphBuilder.start.then(function () {
+  graphBuilder.start.then(function() {
     // todo: check if it supports webgl
-    if(!$scope.$$phase) {
-      $scope.$apply(function () { $scope.canSwitchMode = true;});
+    if (!$scope.$$phase) {
+      $scope.$apply(function() {
+        $scope.canSwitchMode = true;
+      });
     } else {
       $scope.canSwitchMode = true;
+    }
+    var root = graphBuilder.graph.getNode($scope.root);
+    if (root) {
+      selectPackage(root);
     }
   });
 
   function selectPackage(node) {
-    $scope.$apply(function () {
+    if (!$scope.$$phase) {
+      $scope.$apply(function() {
+        $scope.selectedPackage = node.data;
+      });
+    } else {
       $scope.selectedPackage = node.data;
-    });
+    }
   }
 };
