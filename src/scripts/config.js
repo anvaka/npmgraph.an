86b25664a03ad3d4c599f07a59fa611e330a6f63
Andriy Kashcha <anvaka@gmail.com>
2018-04-20 20:56:37 -0700

    Fixed https link
    fixes https://github.com/anvaka/npmgraph.an/issues/23

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   registryUrl: 'https://registry.npmjs.cf/',
-  autoCompleteUrl: 'http://npm-cors-proxy.herokuapp.com/registry/_design/scratch/_view/byField'
+  autoCompleteUrl: 'https://npm-cors-proxy.herokuapp.com/registry/_design/scratch/_view/byField'
 }
